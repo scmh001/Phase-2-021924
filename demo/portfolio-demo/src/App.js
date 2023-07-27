e00332e75c702ebffb138b79d01e987e96bfcb57
Rachel Katz <rachel.katz@flatironschool.com>
2023-07-27 14:26:45 -0400

    restructured files

--- demo/portfolio-demo/src/App.js
+++ demo/portfolio-demo/src/App.js
@@ -1,8 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
 import Header from "./components/Header";
 import ProjectList from "./components/ProjectList";
 import ProjectForm from "./components/ProjectForm";
 
+function App() {
+	const [darkMode, setDarkMode] = useState(true);
+	//💡 what should we intialize projects state with
+	//🛑 has to be [] because we call .filter on projects in ProjectList.js
+	const [projects, setProjects] = useState([]);
+
+	const [phaseState, setPhase] = useState(0);
+
+	function updatePhase(phaseNumber) {
+		setPhase(phaseNumber);
+	}
+
+	const updateDarkMode = () => {
+		setDarkMode((prevDarkMode) => !prevDarkMode);
+	};
+
+	const loadProjects = () => {
+		fetch("http://localhost:4000/projects")
+			.then((res) => res.json())
+			//setProjects causes view to re-render
+			.then((data) => setProjects(data));
+	};
+
+	return (
+		<div className={darkMode ? "App" : "App light"}>
+			<Header handleClick={updateDarkMode} darkMode={darkMode} />
+			<ProjectForm />
+			<button onClick={loadProjects}>Load Projects</button>
+			<ProjectList
+				phaseState={phaseState}
+				updatePhase={updatePhase}
+				projects={projects}
+			/>
+		</div>
+	);
+}
+
+export default App;
 
 /*
 inverse flow
@@ -20,43 +58,3 @@ inverse flow
 - results in setState in parent
 - results in a state change -> re-render
 */
-
-
-function App() {
-
-  const [darkMode, setDarkMode] = useState(true)
-  //has to be [] because we call .filter on projects in ProjectList.js
-  const [ projects, setProjects ] = useState([])
-
-  const [phaseState, setPhase] = useState(0)
-
-  function updatePhase(phaseNumber) {
-		setPhase(phaseNumber)
-	}
-
-
-  const updateDarkMode = () => {
-    setDarkMode(prevDarkMode => !prevDarkMode)
-  }
-
-  const loadProjects = () => {
-    fetch('http://localhost:4000/projects')
-    .then(res => res.json())
-    //setProjects causes view to re-render
-    .then(data => setProjects(data))
-  }
-  
-
-
-  return (
-    <div className={darkMode ? "App" : "App light"}>
-      <Header handleClick={updateDarkMode} darkMode={darkMode} />
-      <ProjectForm />
-      <button onClick={loadProjects}>Load Projects</button>
-      <ProjectList phaseState={phaseState} updatePhase={updatePhase} projects={projects}/>
-    </div>
-  );
-}
-
-
-export default App;
