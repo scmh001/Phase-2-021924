c448d8aa8128b07c51e69f398204f97f2f9ebc6e
Rachel Katz <rachel.katz@flatironschool.com>
2023-11-16 11:38:50 -0500

    removed solution

--- demo/portfolio-demo/src/App.js
+++ demo/portfolio-demo/src/App.js
@@ -3,20 +3,20 @@ import ProjectList from "./components/ProjectList"
 import ProjectForm from "./components/ProjectForm"
 import projects from './projects'
 
+//✅ 1. create child components
+//✅ 1a. create a header component
+//✅ 1b. create a project form component
+//✅ 1c. create a project list component
+//✅ 1d. provide an array of projects to the project list
 
-function App() {
-  return (
-    //✅ 1. create child components
-    //✅ 1a. create a header component
-    //✅ 1b. create a project form component
-    //✅ 1c. create a project list component
-    //✅ 1d. provide an array of projects to the project list
-    <div className="App">
-      <Header />
-      <ProjectForm />
-      <ProjectList projects = {projects}/>
-    </div>
-  );
-}
+//✅ 2. create a project list component
+//✅ 2a. accept the projects array as props
+//✅ 2b. display each project title in an li element
+
+
+//✅ 3. define ProjectListItem
+//✅ 3a. accept the props argument
+//✅ 3b. destructure the props object
+//✅ 3c. display info of project object
 
 export default App;
